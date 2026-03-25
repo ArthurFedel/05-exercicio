073fb5a5ab6fe699fad974ea631eb93769e97d29
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-24 22:42:06 -0300

    ParkEasy em andamento!

---
+++ src/ParkEasy/Controle.java
@@ -0,0 +1,12 @@
+package ParkEasy;
+
+public class Controle {
+    Veiculo veiculo;
+    String horaEntrada;
+    String horaSaida;
+
+    public Controle(Veiculo veiculo, String horaEntrada) {
+        this.veiculo = veiculo;
+        this.horaEntrada = horaEntrada;
+    }
+}
---
+++ src/ParkEasy/Proprietario.java
@@ -0,0 +1,11 @@
+package ParkEasy;
+
+public class Proprietario {
+    String nome;
+    long cpf;
+
+    public Proprietario(String nome, long cpf) {
+        this.nome = nome;
+        this.cpf = cpf;
+    }
+}
